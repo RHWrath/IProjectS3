cfd131e0e05f81d016bfc1dc1b6b4aee281cb172
RHWrath <Rik.Heerholtz@gmail.com>
2024-10-30 13:40:00 +0100

    Unit Cat Test

---
+++ Backend-Example/Backend-Test/LogicTest/CatUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using Logic;
+
+namespace Backend_Test
+{
+    [TestClass]
+    public class CatUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            MockCatDAL mockDAL = new MockCatDAL();
+            CatLogic TestCLogic = new CatLogic(mockDAL);
+
+            //act
+            var TestResult = TestCLogic.GetCats();
+
+            //assert
+            Assert.AreEqual(1, TestResult.Count());
+            var Cat = TestResult.First();
+            Assert.AreEqual("spinx", Cat.Name);
+            Assert.AreEqual("empty", Cat.Description);
+
+        }
+    }
+}
