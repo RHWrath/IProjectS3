4406846fd2a092c01c47f9cafcd76431c2ee8ae6
RHWrath <Rik.Heerholtz@gmail.com>
2024-12-11 09:55:52 +0100

    yep

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ echo -e '\x1b[1;5;97;43m Open the site at https://localhost \x1b[0m'
     (cd Backend/Presentation-Kattencafe && dotnet watch --quiet) &
     (cd Frontend-Klant/ && pnpm start --host --logLevel error) &
     (cd Frontend-Admin/ && pnpm start --host --logLevel error) &
-    (./caddy.exe run > /dev/null 2>&1) ;
+    (./caddy.exe run ) ;	# > /dev/null 2>&1
 kill 0)
