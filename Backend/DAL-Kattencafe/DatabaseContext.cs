2fec1c78c153509e47900d2b905e7b46b5f3aca6
RHWrath <Rik.Heerholtz@gmail.com>
2024-12-23 15:10:12 +0100

    FUCKED

--- Backend/DAL-Kattencafe/DatabaseContext.cs
+++ Backend/DAL-Kattencafe/DatabaseContext.cs
@@ -9,12 +9,13 @@ namespace DAL
         public DbSet<MenuCardModel> MenuCards { get; set; }
         public DbSet<UserAcountModel> UserAcounts { get; set; }
 
-        private const string connection = $"Server=mssqlstud.fhict.local;Database=dbi514798_ips3;user id=dbi514798_ips3;password=SWW#1;TrustServerCertificate=True;";
-
+        private const string devConnection = $"Server=mssqlstud.fhict.local;Database=dbi514798_ips3;user id=dbi514798_ips3;password=SWW#1;TrustServerCertificate=True;";
+        private const string liveConnection = $"Server=mssqlstud.fhict.local;Database=dbi514798_ips3live;user id=dbi514798_ips3live;password=TeLangWachtwoord#1;TrustServerCertificate=True;";
+        
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             var conn = Environment.GetEnvironmentVariable("ConnectionString");
-            if (conn == null) conn = connection;
+            if (conn == null) conn = devConnection;
             options.UseSqlServer(conn);
             
         }
--- Backend/Presentation-Kattencafe/Program.cs
+++ Backend/Presentation-Kattencafe/Program.cs
@@ -1,10 +1,11 @@
 using Main;
 using Main.Controllers;
 using DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DatabaseContext>();
 
 
 // Add services to the container.
@@ -17,6 +18,19 @@ builder.Services.AddCors(policyBuilder =>
     policyBuilder.AddDefaultPolicy(policy =>
         policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod())
 );
+
+var environment = builder.Environment.EnvironmentName;
+var connectionString = environment switch
+{
+    "Development" => builder.Configuration.GetConnectionString("DevelopmentDB"),
+    "Production" => builder.Configuration.GetConnectionString("ProductionDB"),
+    _ => throw new Exception($"Environment {environment} is not supported.")
+};
+
+Console.WriteLine("GET FUCKED");
+
+//builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<DatabaseContext>();
 
 var app = builder.Build();
 
